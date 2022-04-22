b261596b5818d93c610926866b8609e0d70b0622
Risky Farhan <rizky_f20@mhs.unsyiah.ac.id>
2022-04-22 12:17:53 +0800

    hapus file

--- protect.php
+++
@@ -1,7 +0,0 @@
-<!-- membuat proteksi sistem pada setiap sesi login -->
-<?php 
-    if (!isset($_SESSION['login'])) {
-        echo "<script>location='login.php';</script>";
-        exit();
-    }
-?>
